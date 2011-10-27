08c4faf3d856a7039849e23a5a3d39d9dd2a7222
Tim Benniks <tbenniks@gmail.com>
2011-10-27 18:10:31 +0200

    Added dialog

--- install/files/dialog.js
+++ install/files/dialog.js
@@ -5,13 +5,111 @@
 	var dialog = function() 
 	{
 		var	ns = NAMESPACE,
-			mediator = 	ns.Mediator,
+			mediator = ns.Mediator,
+			doc = ns.constants.document,
+			win = ns.constants.window,
+			dialog,
+			dialogContentHolder,
+			dialogCloseBtn,
+			dialogContent = 'Use the setContent function to add content to the dialog.',
+		
+		build = function()
+		{
+			dialog = $('<div id="dialog"></div>').hide();
+			dialogContentHolder = $('<div class="dialog-content-holder"></div>');
+			dialogCloseBtn = $('<div class="dialog-close-btn">close</div>');
 			
+			dialog
+				.append(dialogCloseBtn)
+				.append(dialogContentHolder)
+				.appendTo(doc.find('body'));
+			
+			
+		},
+		
+		position = function()
+		{
+			var dialogWidth = dialog.outerWidth(),
+				dialogHeight = dialog.outerHeight(),
+				windowWidth = win.width(),
+				windowHeight = win.height(),
+				windowScrollTop = win.scrollTop(),
+				windowScrollLeft = win.scrollLeft(),
+				top = ((windowHeight - dialogHeight) / 2),
+				left = ((windowWidth - dialogWidth) / 2);
+			
+			if (windowScrollTop > 0) { top += windowScrollTop; }
+			if (windowScrollLeft > 0) { left += windowScrollLeft; }
+			
+			if(top < 0) { top = windowScrollTop; }
+			if(left < 0) { left = windowScrollLeft; }
+			
+			dialog.css({ top: top, left:left });
+		},
+		
+		fill = function()
+		{
+			dialogContentHolder.html(dialogContent);
+		},
+		
+		setFill = function(content)
+		{
+			dialogContent = content;
+		},
+		
+		hide = function(callback)
+		{
+			if(typeof dialog !== 'undefined')
+			{
+				dialog.fadeOut(300, function()
+				{
+					$(this).remove();
+					mediator.broadcast('DialogHidden');
+					
+					if(callback && typeof callback === 'function')
+			{
+				callback();
+			}
+				});
+			}
+			else 
+			{
+				if(callback && typeof callback === 'function')
+				{
+					callback();
+				}
+			}
+		},
+		
+		bindListeners = function()
+		{
+			dialogCloseBtn.bind('click', hide);
+			win.bind('scroll resize', position);
+		},
+		
+		show = function()
+		{
+			hide(function()
+			{
+				build();
+				bindListeners();
+				fill();
+				position();
+				
+				dialog.fadeIn(300, function()
+				{
+					mediator.broadcast('DialogVisible');
+				});
+			});
+		},
+					
 		listeners = 
 		{
-			onInit: function() 
+			onShowDialog: show,
+			onHideDialog: hide,		
+			setContent: function(content)
 			{
-				
+				setFill(content);
 			}
 		};
 		
@@ -20,4 +118,4 @@
 	
 	NAMESPACE.Mediator.add('dialog', dialog);
 
-})(jQuery, window.NAMESPACE = window.NAMESPACE || {});
+}(jQuery, window.NAMESPACE = window.NAMESPACE || {}));
